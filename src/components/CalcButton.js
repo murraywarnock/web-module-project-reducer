603d06f71bb2d24e1cabf7910c1f1c19b844f0c3
MurrayWarnock <murray.warnock@gmail.com>
2021-06-07 14:29:01 -0600

    "Understanding-Questions.md"" complete.

--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const CalcButton = (props) => {
     const { value, onClick = ()=>{}, size = 4} = props;
-    // const { value, onClick, size = 4} = props;
 
     return(<div className={`col-xs-${size}`}>
         <button type="button" onClick={onClick} className="btn">{value}</button>
